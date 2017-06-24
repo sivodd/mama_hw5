61c73140659b0250d8db2a36a2bf75617358df13
sivan <sivanoddes@gmail.com>
2017-06-24 14:22:23 +0200

    updated Mysharedptr and ignor files

--- MySharedPtr.h
+++ MySharedPtr.h
@@ -2,8 +2,8 @@
 #define _MY_SHARED_PTR_H_
 #include <iostream>
 
-template <class T> 
-class MySharedPtr 
+template <class T>
+class MySharedPtr
 {
 public:
 	MySharedPtr(T* type);
@@ -11,69 +11,74 @@ public:
 	virtual ~MySharedPtr();
 	T* get();
 	T& operator*();
-	/*T* opertaor->();*/
-	//MySharedPtr<T>& opertaor = (const MySharedPtr<T>& ptr);
+	T* operator->();
+	void operator=(const MySharedPtr<T>& ptr);
 private:
 	int* counter_;
 	T* pointer_;
 };
-//
-//template <class T>
-//MySharedPtr<T>::MySharedPtr(T* type)
-//{
-//	new int *counter_ = 1;
-//	pointer_ = type;
-//}
-//
-//
-//template <class T>
-//MySharedPtr<T>::MySharedPtr(MySharedPtr& ptr)//const?
-//{
-//	counter_ = ptr.pointer;
-//	*counter_++;
-//	pointer_ = ptr.pointer;
-//}
-//
-//template<class T>
-//MySharedPtr<T>::~MySharedPtr()
-//{
-//	*counter--;
-//	if (*counter_ == 0)
-//	{
-//		free(pointer_);
-//		free(counter_);
-//	}
-//}
-//
-//template <class T>
-//T* MySharedPtr<T> ::get()
-//{
-//	return pointer_;
-//}
-//
-//template <class T>
-//T& MySharedPtr<T>::operator*()
-//{
-//	return *pointer_;
-//}
-//
-//template <class T>
-//T* MySharedPtr<T>::operator->()
-//{
-//	return pointer_;
-//}
-//
-//template <class T>
-//MySharedPtr& MySharedPtr<T>::operator=(const MySharedPtr& ptr) 
-//{
-//	if (this == &ptr)
-//		return *this;
-//	(*counter_)--;
-//	delete this;
-//	MySharedPtr p = new MySharedPtr(ptr);
-//	//return *p;
-//	return this;
-//}
+
+template <class T>
+MySharedPtr<T>::MySharedPtr(T* type)
+{
+	counter_ = new int;
+    *counter_= 1;
+	pointer_ = type;
+}
+
+
+template <class T>
+MySharedPtr<T>::MySharedPtr(MySharedPtr& ptr)//const?
+{
+	counter_ = ptr.counter_;
+    (*counter_)++;
+	pointer_ = ptr.pointer_;
+}
+
+template<class T>
+MySharedPtr<T>::~MySharedPtr()
+{
+    (*counter_)--;
+	if (*counter_ == 0)
+	{
+		free(pointer_);
+		free(counter_);
+	}
+}
+
+template <class T>
+T* MySharedPtr<T> ::get()
+{
+	return pointer_;
+}
+
+template <class T>
+T& MySharedPtr<T>::operator*()
+{
+	return *pointer_;
+}
+
+template <class T>
+T* MySharedPtr<T>::operator->()
+{
+	return pointer_;
+}
+
+template <class T>
+void MySharedPtr<T>::operator=(const MySharedPtr<T>& ptr)
+{
+	if (this != &ptr)
+    {
+        (*counter_)--;
+        if (counter_==0){
+            free(counter_);
+            free(pointer_);
+        }
+        counter_ = ptr.counter_;
+        (*counter_)++;
+        pointer_ = ptr.pointer_;
+    }
+}
 
 
 #endif // _MY_SHARED_PTR_H_
