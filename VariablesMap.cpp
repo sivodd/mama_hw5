ed58ed525811950db7b3f9ce1ba7a088c8351acf
sivan <sivanoddes@gmail.com>
2017-06-26 23:00:08 +0200

    updated variablesmaps

--- VariablesMap.cpp
+++ VariablesMap.cpp
@@ -1,7 +1,54 @@
 #include"VariablesMap.h"
 #include"ScriptExceptions.h"
+#include <string>
+#include <string.h>
+
+using namespace std;
+map<string, Variable>VarMap;
+
+VariablesMap::VariablesMap(): {
+//    map<string, VarPtr>VarMap; //not sure this should be varptr maybe just variable*
+    VarMap.clear();
+    string saved_name("");
+    savedName_=saved_name;
 
 
+};
+
+
+void VariablesMap::SetSavedName(const string& newSavedName){
+    if (newSavedName!=NULL)
+    savedName_=newSavedName;
+}
+
+char* VariablesMap::GetTmpVariable(){}
+
+void VariablesMap::ClearTmpVars(){
+
+}
+Variable& VariablesMap::operator[](const string& x){
+    bool flag= true;
+    string s=VALID_VAR_CHARS;
+    auto_ptr < Variable > p_var(new Variable);
+    if (isalpha(x[0])){
+        for (int i=1; i<x.length(); i++){
+            if (s.find(x[i])==string::npos) {
+                flag=false;
+                break;
+            }
+        }
+        if (flag){
+            p_var=NULL;
+            return VarMap[string(x)]=p_var;
+        }
+    }
+    throw (INVALID_VAR_NAME(x));
+}
+Variable& VariablesMap::at(const string& x){
+    return VarMap.at(x);
+}
+
+void VariablesMap::erase(const string& x);
 
 
 
--- VariablesMap.h
+++ VariablesMap.h
@@ -1,12 +1,30 @@
 #ifndef _VARIABLES_MAP_H_
 #define _VARIABLES_MAP_H_
 
-
+#include <iostream>
+#include <string>
+#include <map>
+#include <memory>
+#include "Variable.h"
+using namespace std;
 
 
 #define VALID_VAR_CHARS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
+typedef auto_ptr<Variable> p1 (new Variable);
+class VariablesMap {
+public:
+    VariablesMap();
+    ~VariablesMap(){string.~string();};// i think string doesn't need to be deleted because it's destructor is automatically called
+    void SetSavedName(const string& newSavedName);
+    char* GetTmpVariable();
+    void ClearTmpVars();
+    variable& operator[](const string& x);
+    variable& at(const string& x);
+    void erase(const string& x);
 
-
+private:
+    string savedName_;
+};
 
 
 #endif // _VARIABLES_MAP_H_
