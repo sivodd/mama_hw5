498325a518bd20d6caa4d777b024ce73ad4d72c1
sivan <sivanoddes@gmail.com>
2017-06-27 11:26:20 +0200

    updated VariablesMap

--- VariablesMap.cpp
+++ VariablesMap.cpp
@@ -1,35 +1,46 @@
 #include"VariablesMap.h"
 #include"ScriptExceptions.h"
-#include <string>
-#include <string.h>
 
 using namespace std;
-map<string, Variable>VarMap;
 
 VariablesMap::VariablesMap(): {
-//    map<string, VarPtr>VarMap; //not sure this should be varptr maybe just variable*
-    VarMap.clear();
+    VariablesMap_.clear();
     string saved_name("");
     savedName_=saved_name;
-
-
 };
 
 
 void VariablesMap::SetSavedName(const string& newSavedName){
     if (newSavedName!=NULL)
-    savedName_=newSavedName;
+        savedName_=newSavedName;
 }
 
-char* VariablesMap::GetTmpVariable(){}
+string VariablesMap::GetTmpVariable(){
+    srand((unsigned)time(0)); // use current time as seed for random generator
+    string s= VALID_VAR_CHARS;
+    int floor = 0, ceiling = int(s.length()), range = (ceiling - floor);
+    string tmp("a");
+    while (VariablesMap_.find(tmp) == VariablesMap_.end()){
+        int rnd = floor + int((range * rand()) / (RAND_MAX + 1.0));
+        tmp= s[rnd] + tmp;
+    }
+    VariablesMap_[tmp]=NULL;// WE ADD THE TMP THAT WE FOUND TO THE MAP
+    tmp_list.push_back(tmp);
+    return tmp;
+}
 
 void VariablesMap::ClearTmpVars(){
-
+    while (!tmp_list.empty())
+    {
+        string tmp;
+        tmp = tmp_list.front();
+        VariablesMap_.erase(tmp);
+        tmp_list.pop_front();
+    }
 }
-Variable& VariablesMap::operator[](const string& x){
+VarPtr& VariablesMap::operator[](const string& x){
     bool flag= true;
     string s=VALID_VAR_CHARS;
-    auto_ptr < Variable > p_var(new Variable);
     if (isalpha(x[0])){
         for (int i=1; i<x.length(); i++){
             if (s.find(x[i])==string::npos) {
@@ -38,17 +49,19 @@ Variable& VariablesMap::operator[](const string& x){
             }
         }
         if (flag){
-            p_var=NULL;
-            return VarMap[string(x)]=p_var;
+            return VariablesMap_[x]= NULL;//NOT SURE
         }
     }
     throw (INVALID_VAR_NAME(x));
 }
-Variable& VariablesMap::at(const string& x){
-    return VarMap.at(x);
+VarPtr& VariablesMap::at(const string& x){
+    return VariablesMap_.at(x);
 }
 
-void VariablesMap::erase(const string& x);
+void VariablesMap::erase(const string& x){
+    VariablesMap_.erase(x);
+    tmp_list.erase(tmp);//need to erase tmp from list if it exists.
+}
 
 
 
