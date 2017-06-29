d09ea3b5ffb24beb95d7cdee737a0cfbd36e10db
sivan <sivanoddes@gmail.com>
2017-06-29 12:11:38 +0200

    updeted variablesmaps

--- VariablesMap.cpp
+++ VariablesMap.cpp
@@ -20,7 +20,8 @@ string VariablesMap::GetTmpVariable(){
     string s= VALID_VAR_CHARS;
     int floor = 0, ceiling = int(s.length()), range = (ceiling - floor);
     string tmp("a");
-    while (VariablesMap_.find(tmp) == VariablesMap_.end()){
+//  while tmp is already in the map or the saved name continue looking for a key.
+    while (VariablesMap_.find(tmp) != VariablesMap_.end() or tmp==savedName_){
         int rnd = floor + int((range * rand()) / (RAND_MAX + 1.0));
         tmp= s[rnd] + tmp;
     }
@@ -49,18 +50,18 @@ VarPtr& VariablesMap::operator[](const string& x){
             }
         }
         if (flag){
-            return VariablesMap_[x]= NULL;//NOT SURE
+            return VariablesMap_[x]= NULL;
         }
     }
     throw (INVALID_VAR_NAME(x));
 }
-VarPtr& VariablesMap::at(const string& x){
-    return VariablesMap_.at(x);
-}
+//default map method:
+//VarPtr& VariablesMap::at(const string& x){}
 
 void VariablesMap::erase(const string& x){
     VariablesMap_.erase(x);
-    tmp_list.erase(tmp);//need to erase tmp from list if it exists.
+    if (find(tmp_list.begin(), tmp_list.end(), x) != tmp_list.end())
+        tmp_list.remove(x);//need to erase tmp from list if it exists.
 }
 
 
